2c87cdbdc329ee488d203cee4acca7141649994c
Jaco Van Zyl <jvanzyl@SINGULAR.CO.ZA>
2021-06-21 23:22:55 +0200

    59. Lifting the state up

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
 import './ExpenseItem.css';
+import React, {useState} from 'react'; // React hook
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 const ExpenseItem =(props)=> {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler=()=>{
+    setTitle('Updated');
+  }
+  
   return (
     <Card className ="expense-item">
       <ExpenseDate date ={props.date} />
       <div className ="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2 className="expense-item__price">R{props.amount}</h2>
       </div>
+      <button onClick = {clickHandler}>change title</button>
     </Card>
   );
 }
